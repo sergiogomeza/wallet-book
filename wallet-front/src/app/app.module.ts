ed4140f22141a1c432e1c90bbe71d1d8494e8650
Sergio <sgomez@cidenet.com.co>
2020-01-29 23:51:51 -0500

    creacion de componentes
    Se crean componentes de la vista que seran modificados para ir agregando la funcionalidad esperada

--- wallet-front/src/app/app.module.ts
+++ wallet-front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovementListComponent } from './movement-list/movement-list.component';
+import { BudgetInfoComponent } from './budget-info/budget-info.component';
+import { AccountDataComponent } from './account-data/account-data.component';
+import { AccountInfoComponent } from './account-info/account-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovementListComponent,
+    BudgetInfoComponent,
+    AccountDataComponent,
+    AccountInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ wallet-front/src/app/budget-info/budget-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-info',
+  templateUrl: './budget-info.component.html',
+  styleUrls: ['./budget-info.component.css']
+})
+export class BudgetInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
